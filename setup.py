3430f15a22659ad3e2869562444c1ff8e83ba00d
Doug Turnbull <softwaredoug@gmail.com>
2016-01-14 13:40:12 -0500

    Rev to 0.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         ]
     },
     packages=['elyzer'],
-    version="0.1.1",
+    version="0.1.2",
     description="Step-by-Step Debug Elasticsearch Analyzers",
     long_description=read_md('README.md'),
     license="Apache",
