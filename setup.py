ece34c65973363285c10979a9eb61f1bd77f2d7b
Doug Turnbull <softwaredoug@gmail.com>
2016-02-21 22:45:52 -0500

    Revs to 0.2.0 for some built in analyzers

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         ]
     },
     packages=['elyzer'],
-    version="0.1.2",
+    version="0.2.0",
     description="Step-by-Step Debug Elasticsearch Analyzers",
     long_description=read_md('README.md'),
     license="Apache",
