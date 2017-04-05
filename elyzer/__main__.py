dedaaed7cce79d8a75ed07aedd825be040ea5b35
Doug Turnbull <softwaredoug@gmail.com>
2017-04-05 09:48:05 -0400

    Treats text as positional arg; uses envvars

--- elyzer/__main__.py
+++ elyzer/__main__.py
@@ -4,17 +4,23 @@ urllib3.disable_warnings()
 import argparse
 from elasticsearch import Elasticsearch
 from elyzer import stepWise, getAnalyzer
+from envDefault import EnvDefault
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--es', type=str,
-                        help='Root URL to Elasticsearch, ie http://localhost:9200',
+                        help='Root URL to Elasticsearch, ie http://localhost:9200 (defaults to envvar ELYZER_ES_URL or localhost:9200)',
+                        action=EnvDefault,
+                        required=True,
+                        envvar='ELYZER_ES_URL',
                         default='http://localhost:9200')
-    parser.add_argument('--index', type=str, required=True,
-                        help='Name of the index to find the analyzer, ie tweets')
-    parser.add_argument('--analyzer', type=str, required=True,
-                        help='Name of the custom analyzer, ie my_text_analyzer')
-    parser.add_argument('--text', type=str, required=True,
+    parser.add_argument('--index', type=str, action=EnvDefault,
+                        required=True, envvar='ELYZER_INDEX',
+                        help='Name of the index to find the analyzer, ie tweets (defaults to envvar ELYZER_INDEX)')
+    parser.add_argument('--analyzer', type=str, action=EnvDefault, required=True,
+                        envvar='ELYZER_ANALYZER',
+                        help='Name of the custom analyzer, ie my_text_analyzer (defaults to envvar ELYZER_ANALYZER)')
+    parser.add_argument('text', type=str,
                         help='Text to analyze, ie "mary had a little lamb"')
     return vars(parser.parse_args())
 
---
+++ elyzer/envDefault.py
@@ -0,0 +1,14 @@
+import argparse
+import os
+
+class EnvDefault(argparse.Action):
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if envvar in os.environ:
+            default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required,
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
